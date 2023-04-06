b417344d515cbc828a2f138c5d716ccc6f32aed9
kajMork <kmark19@student.aau.dk>
2023-04-06 22:13:59 +0200

    Updated cell class to handle PackML states

--- cell_class.py
+++ cell_class.py
@@ -9,12 +9,25 @@ import json_telegrams
 # class that represents the pallet cell
 
 class Cell:
-    def __init__(self, name):
-        self.name = name
-        self.status = "Idle"
+    def __init__(self, id):
+        # Cell specific variables
+        self.cell_id = id
+        self.cell_prefix = "palletcells"
         self.client = None
-        #self.loop = asyncio.get_event_loop()
-        self.HCL_start_layer_topic = "HCL/1/palletize/start_layer"
+        
+        # MQTT topics
+        self.HCl_prefix = "HCL"
+        self.HCL_id = "1"
+        self.HCL_start_layer_topic = self.HCl_prefix + "/" + self.HCL_id + "/palletize/start-layer"
+        self.HCL_state_request_topic = self.HCl_prefix + "/" + self.HCL_id + "/state/request"
+        self.HCL_state_feedback_topic = self.cell_prefix + "/" + self.cell_id + "/system/state"
+        
+        
+        # System state variables
+        self.old_state = "Offline"
+        self.state = "Idle"
+        self.system_state = json_telegrams.system_state(self.cell_prefix, self.cell_id, self.client, self.state)
+        self.HCL_state_request = False
        
     # 5.5.1 Start Palletizing Single Layer (HLC -> PLC)
     async def start_layer(self):
@@ -25,12 +38,28 @@ class Cell:
                     print("Starting layer with layer-pattern id : " + str(obj["layer-pattern-id"]))
                     print("Starting layer with height-offset : " + str(obj["height-offset"]))
                     print("Sent at : " + str(obj["timestamp"]))
-                    await self.client.publish("HCL_feedback", "Starting layer")
-                    self.status = "Working"
+                    self.state = "Starting"
+                    await asyncio.sleep(5)
+                    print("Layer started. state = " + self.state)
+                    self.state = "Execute"
                     await asyncio.sleep(15)
-                    await self.client.publish("HCL_feedback", "Layer done")
-                    self.status = "Idle"
-                    print("Layer done")
+                    self.state = "Complete"
+                    print("Layer done. state = " + self.state)
+
+    async def state_update(self):
+        while True:
+            if self.state != self.old_state or self.HCL_state_request == True:
+                print("old state: " + self.old_state)
+                self.old_state = self.state
+                print("new state: " + self.old_state)
+                #self.system_state.state = self.state
+                temp_state = json_telegrams.system_state(self.cell_prefix, self.cell_id, self.client, self.state)
+                print("Sending status", self.system_state.state)
+                #await self.system_state.send_telegram()
+                await temp_state.send_telegram()
+                self.HCL_state_request = False
+            await asyncio.sleep(0.5)
+
     
     async def test_listen(self):
         async with self.client.messages() as messages:
@@ -48,10 +77,12 @@ class Cell:
             subscribe_handler_task = loop.create_task(self.subcribe_handler())
             start_layer_task = loop.create_task(self.start_layer())
             listen_task = loop.create_task(self.test_listen())
-            
+            #state_update_task = loop.create_task(self.state_update())
+            state_update_task = asyncio.ensure_future(self.state_update())
             await listen_task
             await start_layer_task
             await subscribe_handler_task
+            #await state_update_task
         
     
     
