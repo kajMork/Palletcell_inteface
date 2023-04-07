0841db4f93a8a34ac9d1d750e4b5933f1fdef35d
kajMork <kmark19@student.aau.dk>
2023-04-07 14:36:51 +0200

    Added OPC-UA server function

--- cell_class.py
+++ cell_class.py
@@ -1,13 +1,15 @@
 import json
 import paho.mqtt.client as mqtt_client
 import asyncio_mqtt as aiomqtt
-
+from asyncua import ua, Server
+from asyncua.common.methods import uamethod
 import asyncio
 import time
-
+import logging
 # JSON telegram template classes
 import json_telegrams
-
+import sys
+sys.path.insert(0, "..")
 # TODO:
 # 1. Implement suspend and aborted states and how to handle these requests from HCL.
 # 2. Implement the request of partial layer palletization Page 28, chapter 6.2.
@@ -58,12 +60,10 @@ class Cell:
     async def state_update(self):
         while True:
             if self.state != self.old_state or self.HCL_state_request == True:
-                print("old state: " + self.old_state)
                 self.old_state = self.state
-                print("new state: " + self.old_state)
                 #self.system_state.state = self.state
                 temp_state = json_telegrams.system_state(self.cell_prefix, self.cell_id, self.client, self.state)
-                print("Sending status", self.system_state.state)
+                print("Sending new state", self.system_state.state)
                 #await self.system_state.send_telegram()
                 await temp_state.send_telegram()
                 self.HCL_state_request = False
@@ -79,7 +79,42 @@ class Cell:
     async def subcribe_handler(self):
         await self.client.subscribe(self.HCL_start_layer_topic)
         await self.client.subscribe("palletcell2")
+    
+    async def opc_ua_handler(self):
+
+        @uamethod
+        def func(parent, value):
+            return value * 2
+        _logger = logging.getLogger('asyncua')
+        # setup our server
+        server = Server()
+        await server.init()
+        server.set_endpoint('opc.tcp://192.168.12.246:4840/server/')
+
+        # setup our own namespace, not really necessary but should as spec
+        uri = 'http://examples.freeopcua1.github.io'
+        idx = await server.register_namespace(uri)
+
+        # populating our address space
+        # server.nodes, contains links to very common nodes like objects and root
+        myobj = await server.nodes.objects.add_object(idx, 'MyObject')
+        myvar = await myobj.add_variable(idx, 'MyVariable', str(1))
         
+        await myvar.set_writable()
+        
+        await server.nodes.objects.add_method(ua.NodeId('ServerMethod', 2), ua.QualifiedName('ServerMethod', 2), func, [ua.VariantType.Int64], [ua.VariantType.Int64])
+        _logger.info('Starting server!')
+        async with server:
+            while True:
+                await asyncio.sleep(1)
+                if self.state == "Execute":
+                    _logger.info('Set value of %s to %.s', myvar, self.state)
+                    await myvar.write_value(self.state)
+                elif self.state == "Complete":
+                    _logger.info('Set value of %s to %.s', myvar, self.state)
+                    await myvar.write_value(self.state)
+                
+    
     async def main(self):
         loop = asyncio.get_event_loop()
         async with aiomqtt.Client("localhost") as self.client:
@@ -88,6 +123,9 @@ class Cell:
             listen_task = loop.create_task(self.test_listen())
             #state_update_task = loop.create_task(self.state_update())
             state_update_task = asyncio.ensure_future(self.state_update())
+            opc_ua_task = asyncio.create_task(self.opc_ua_handler())
+            
+            await opc_ua_task
             await listen_task
             await start_layer_task
             await subscribe_handler_task
