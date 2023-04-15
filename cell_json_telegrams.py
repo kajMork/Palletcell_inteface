d8740cb609e20582a9b9a78dfd8d36925e9b86bb
kajMoerk <kmark19@student.aau.dk>
2023-04-15 22:26:28 +0200

    Added new telegram classes and funcs

--- cell_json_telegrams.py
+++ cell_json_telegrams.py
@@ -1,14 +1,31 @@
 import utils
 import json
 import glob
-# TODO: add a class for each telegram type
-alarm_telegram = {
-    "version": "v1.0",
-    "timestamp": utils.get_datetime(),
-    "alarm-id": None,
-    "alarm-text": None,
-    "active": None
-}
+import asyncio
+# 4.3 Error Message
+async def error_message(topic, client, error_id):
+    """Send error message to HLC, based on error_id.
+
+    Args:
+        topic (string): 
+        client (_type_): _description_
+        error_id (string): Supports: ERR_UNSUPPORTED, ERR_INVALID_MESSAGE, ERR_INVALID_STATE
+    """
+    description = None
+    
+    if error_id == "ERR_UNSUPPORTED":
+        description = "Received request cannot be processed as it is not supported by the palletizing cell (e.g.responding with System Capabilities)"
+    elif error_id == "ERR_INVALID_MESSAGE":
+        description = "Request could not be parsed"
+    elif error_id == "ERR_INVALID_STATE":
+        description = "Palletizing cell is not in a state to execute the request (e.g. stopping an on-goingpalletizing if the cell is in an idle state)"
+        
+    error_message = {
+        "version": "v1.0",
+        "error": error_id,
+        "reason": description,
+    }
+    await client.publish(topic, json.dumps(error_message)) # Send telegram
 
 #5.2.1 System State (Cell -> HLC)
 #Sent by the palletizing cell when state has changed or when requested by the HLC.
@@ -46,13 +63,90 @@ class system_state:
         await self.client.publish(self.topic_name, json.dumps(telegram)) # Send telegram
         del telegram # Delete the telegram to save memory
 
+# 5.2.3 Alarm States (Cell -> HLC) Telegram transmitted by the palletizing cell when the alarm status alters.
+#The alarms are configured on the palletizing cell and HLC and are not part of this specification.
+
+async def add_alarm_state(prefix, cell_id, client, alarm_id, alarm_text, active, alarm_system):
+    """Adds alarm to alarm system and sends telegram to HLC. Alarms types are not yet defined.
+
+    Args:
+        prefix (String): Prefic for the topic
+        cell_id (String): ID of the cell
+        client (Class): client class
+        alarm_id (Int): ID of the alarm
+        alarm_text (String): Text associated with the alarm.
+        active (Boolean): Indicates if the alarm is active (true) or inactive (false).
+        alarm_system (Class): Class for where the alarm should be stored.
+    """
+    
+    # Topic specific variables
+    topic_name = prefix + "/" + cell_id + "/system/alarms"
+    
+    # Telegram specific variables
+    version = "v1.0"
+    # Create the telegram
+    alarm_state_telegram = {
+        "version": version,
+        "timestamp": utils.get_datetime(),
+        "alarm_id": alarm_id,
+        "alarm_text": alarm_text,
+        "active": active
+    }
+    # Add alarm to alarm system
+    await alarm_system.add_alarm(alarm_id, alarm_text, active)
+    
+    # Send telegram
+    await client.publish(topic_name, json.dumps(alarm_state_telegram))
+
+# 5.2.4 Active Alarms (Cell -> HLC)
+# Telegram transmitted by the palletizing cell when a corresponding request telegram is sent.
+# The telegram contains an array of all active alarms.
+# The alarms are configured on the palletizing cell and HLC and are not part of this specification.
+
+class active_alarms:
+    def __init__(self, prefix, cell_id, client):
+        # Topic specific variables
+        self.prefix = prefix + "/"
+        self.cell_id = cell_id + "/"
+        self.topic_name = self.prefix + self.cell_id + "system/active-alarms"
+        self.client = client
+        
+        self.telegram_template = {
+            "version": "v1.0",
+            "timestamp": utils.get_datetime(),
+            "alarms": []
+        }
+        
+    async def send_telegram(self):
+        # update timestamp
+        self.telegram_template["timestamp"] = utils.get_datetime()
+        # Send telegram
+        await self.client.publish(self.topic_name, json.dumps(self.telegram_template))
+        
+    async def add_alarm(self, alarm_id, alarm_text, active):
+        alarm = {
+            "alarm-id": alarm_id,
+            "alarm-text": alarm_text,
+            "active": active
+        }
+        # If alarm is is not active, remove it from the list
+        if not active:
+            for alarm in self.telegram_template["alarms"]:
+                if alarm["alarm-id"] == alarm_id:
+                    self.telegram_template["alarms"].remove(alarm)
+        else:
+            # Only add alarm if it is not already in the list
+            if alarm not in self.telegram_template["alarms"]:
+                self.telegram_template["alarms"].append(alarm)
+                
+
 #5.3.1 Get Layer Pattern (Cell -> HLC)
 #Sent by the palletizing cell when a layer pattern has changed or when requested by the HLC.
 class send_layer_pattern:
     def __init__(self, prefix, cell_id, client):
         # Topic specific variables
-        self.prefix = prefix+"/"
-        self.cell_id = cell_id+"/"
+        self.prefix = prefix
+        self.cell_id = cell_id
         self.topic_name = self.prefix + self.cell_id + "layer-pattern"
         
         # MQTT specific variables
@@ -61,13 +155,27 @@ class send_layer_pattern:
         self.obj = None
         self.container_pattern = None
         
-    async def get_telegram(self):
-        await self.client.subscribe(self.topic_name)
-        async with self.client.messages() as messages:
-            async for message in messages:
-                if message.topic.matches(self.topic_name):
-                    obj = json.loads(message.payload)
-                    container_pattern = obj["containers"]
+        self.telegram_template = {
+            "version": "v1.0",
+            "patterns": [],
+            "timestamp": utils.get_datetime()
+        }
+        
+        
+    async def send_telegram(self):
+        pattern_files = glob.glob("layer_patterns/*.json")
+        telegram = self.telegram_template.copy()
+        for pattern_file in pattern_files:
+            pattern_json = json.load(open(pattern_file))
+            # Remove the version from the pattern
+            pattern_json.pop("version")
+            telegram["patterns"].append(pattern_json)
+        print(json.dumps(telegram))
+        print("Sending layer pattern on topic: "+self.topic_name)
+        await self.client.publish(self.topic_name, json.dumps(telegram)) # Send telegram
+        
+            
+        
 
 
 #5.3.2 Add/Update Layer Pattern (HLC -> Cell)
@@ -93,6 +201,12 @@ class add_update_layer_pattern:
                     with open(pattern_file_name, "w+") as file:
                         json.dump(obj, file)
                     print("Layer pattern file updated: "+pattern_file_name)
+                    # Close the file
+                    file.close()
+                    # Send  the layer pattern back to the HLC to confirm that it was received
+                    send_layer_pattern_class = send_layer_pattern(self.prefix, self.cell_id, self.client)
+                    await send_layer_pattern_class.send_telegram()
+                    
 
 
 # 5.4.1 Container at ID Point (Cell -> HCL)
