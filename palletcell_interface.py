88bf64d2e7d0cd624dee8c3b613c06de6723d9eb
kajMoerk <kmark19@student.aau.dk>
2023-04-15 22:27:27 +0200

    Removed unused code, and added windows support

--- palletcell_interface.py
+++ palletcell_interface.py
@@ -8,81 +8,26 @@
 #$ bin/vernemq start
 import paho.mqtt.client as mqtt_client
 import asyncio_mqtt as aiomqtt
+#import aiomqtt as aiomqtt
 import cell_class
 import asyncio
 import time
+import sys
+import os
 
-status = "Idle"
+#async def main():
+print("Starting main")
 
-async def do_work(client):
-    print("Doing work")
-    
-    t1 = time.time()
-    await client.publish("HCL_feedback", "Began work at")
-    status = "Working"
-    await asyncio.sleep(15)
-    t2 = time.time()
-    await client.publish("HCL_feedback", "Work done at " + str(t2- t1))
-    print("Work done")
-    status = "Idle"
+#asyncio.get_event_loop().run_until_complete(new_cell.main())
+if sys.platform.lower() == "win32" or os.name.lower() == "nt":
+    # only import if platform/os is win32/nt, otherwise "WindowsSelectorEventLoopPolicy" is not present
+    from asyncio import (
+        set_event_loop_policy,
+        WindowsSelectorEventLoopPolicy
+    )
+    # set the event loop
+    set_event_loop_policy(WindowsSelectorEventLoopPolicy())
 
-async def listen(client):
-    temp_task = None
-    async with client.messages() as messages:
-        async for message in messages:
-            feedback = message.payload.decode()
-            print(feedback)
-            if feedback == "start":
-                print("Starting work")
-                temp_task = asyncio.create_task(do_work(client)) # This allows for multiple tasks to be created
-                print("Ending work, feedback sent")
-            elif feedback == "status":
-                print("Sending status")
-                await client.publish("Cell_status", "test")
-                print("Status sent")
-            if temp_task != None:
-                if temp_task.done():
-                    print("Work done")
-                    await client.publish("HCL_feedback", "Work done")
-                    status = "Idle"
-                    temp_task = None
-                else:
-                    print("Work not done")
-                    await client.publish("HCL_feedback", "Work not done")
-                    status = "Working"
-            else:
-                print("No task")
-                await client.publish("HCL_feedback", "No task started")
-                status = "Idle"
-            
-                
-            
+new_cell = cell_class.Cell("palletcells","1")
 
-
-async def setup(client):
-    print('Connected to mqtt broker')
-
-    # Subscribe to topics
-    await client.subscribe("palletcell2")
-    await client.subscribe("palletcell")
-
-
-
-async def main():
-    # Wait for messages in (unawaited) asyncio task
-    loop = asyncio.get_event_loop()
-    async with aiomqtt.Client("localhost") as client:
-        task0 = loop.create_task(setup(client))
-        task = loop.create_task(listen(client))
-        # This will still run!
-        print("Magic!")
-        
-        
-        # If you don't await the task here the program will simply finish.
-        # However, if you're using an async web framework you usually don't have to await
-        # the task, as the framework runs in an endless loop.
-        await task
-        await task0
-
-new_cell = cell_class.Cell("1")
 asyncio.run(new_cell.main())
